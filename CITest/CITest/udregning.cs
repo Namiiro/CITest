edb577eb21a87a210e1b6e9b29b143d0537a8d9d
Jackie Rose Namiiro <35915298+Namiiro@users.noreply.github.com>
2018-08-20 10:14:34 +0200

    Opsætning

---
+++ CITest/CITest/udregning.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITest
+{
+    class udregning
+    {
+        public int Plus(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Minus(int x, int y)
+        {
+            return x - y;
+        }
+        
+    }
+}
